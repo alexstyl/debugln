cd036b2b11f494cd80aedd54ecc5f27baa522826
alexstyl <1665273+alexstyl@users.noreply.github.com>
2025-07-16 11:34:45 +0700

    Add logging level

--- debugln/src/commonMain/kotlin/DebugLn.kt
+++ debugln/src/commonMain/kotlin/DebugLn.kt
@@ -1,6 +1,17 @@
 package com.alexstyl.debugln
 
 var allowLogging: Boolean = true
+var loggingLevel: LoggingLevel = LoggingLevel.VERBOSE
+
+class LoggingLevel(val priority: Int) {
+    companion object {
+        val VERBOSE = LoggingLevel(0)
+        val DEBUG = LoggingLevel(1)
+        val INFO = LoggingLevel(2)
+        val WARN = LoggingLevel(3)
+        val ERROR = LoggingLevel(4)
+    }
+}
 
 private const val COLOR_RED = "\u001b[31m"
 private const val COLOR_AQUA = "\u001b[36m"
@@ -9,32 +20,32 @@ private const val COLOR_ORANGE = "\u001b[38;2;255;165;0m"
 private const val COLOR_RESET = "\u001b[0m"
 
 fun debugln(message: () -> String) {
-    if (allowLogging) {
+    if (allowLogging && loggingLevel.priority <= LoggingLevel.DEBUG.priority) {
         println(message())
     }
 }
 
 fun verboseln(message: () -> String) {
-    if (allowLogging) {
+    if (allowLogging && loggingLevel.priority <= LoggingLevel.VERBOSE.priority) {
         println(message(), COLOR_LIGHT_GRAY)
     }
 }
 
 
 fun infoln(message: () -> String) {
-    if (allowLogging) {
+    if (allowLogging && loggingLevel.priority <= LoggingLevel.INFO.priority) {
         println(message(), COLOR_AQUA)
     }
 }
 
 fun warnln(message: () -> String) {
-    if (allowLogging) {
+    if (allowLogging && loggingLevel.priority <= LoggingLevel.WARN.priority) {
         println(message(), COLOR_ORANGE)
     }
 }
 
 fun errorln(message: () -> String) {
-    if (allowLogging) {
+    if (allowLogging && loggingLevel.priority <= LoggingLevel.ERROR.priority) {
         println(message(), COLOR_RED)
     }
 }
--- sample/src/jvmMain/kotlin/Main.kt
+++ sample/src/jvmMain/kotlin/Main.kt
@@ -1,7 +1,9 @@
+import com.alexstyl.debugln.LoggingLevel
 import com.alexstyl.debugln.allowLogging
 import com.alexstyl.debugln.debugln
 import com.alexstyl.debugln.errorln
 import com.alexstyl.debugln.infoln
+import com.alexstyl.debugln.loggingLevel
 import com.alexstyl.debugln.verboseln
 import com.alexstyl.debugln.warnln
 
@@ -13,8 +15,16 @@ fun main() {
     warnln { "Warn" }
     errorln { "Error" }
 
+    // this will print only debug and above
+    loggingLevel = LoggingLevel.DEBUG
+    verboseln { "Verbose" }
+    debugln { "Debug" }
+    infoln { "Info" }
+    warnln { "Warn" }
+    errorln { "Error" }
+
+    // this turns off all logging
     allowLogging = false
-    // these won't be printed in the console
     verboseln { "Verbose" }
     debugln { "Debug" }
     infoln { "Info" }
